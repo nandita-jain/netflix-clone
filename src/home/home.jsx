98797d8beea1a2d2bf143cfa8c6978d9b2944b47
nandita-jain <jainnandita026@gmail.com>
2022-06-03 11:48:10 +0530

    components done

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import "./Home.scss";
 import Featured from "../components/featured/Featured";
 import Navbar from "../components/navbar/Navbar";
+import List from "../components/list/List";
 
 function home() {
   return (
     <div className="home">
       <Navbar />
-
       <Featured />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 }
