c99bbd734d33fd11f14555b1a5fb0c295dd45f91
nandita-jain <jainnandita026@gmail.com>
2022-06-03 13:00:42 +0530

    ui complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import "./App.scss";
-import Home from "./Home/Home";
+import "./App.scss"
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
 
-const App =()=>{
-  return <Home/>;
+const App = () => {
+  return <Home />;
 };
 
 export default App;
